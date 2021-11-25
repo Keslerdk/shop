5a09a686e66934aa6fd03ecbb232e82d5855d766
keslerdk <kd7614@yandex.ru>
2021-11-25 19:45:48 +0300

    future builder

--- lib/screens/order_screen.dart
+++ lib/screens/order_screen.dart
@@ -15,38 +15,49 @@ class OrderScreen extends StatefulWidget {
 }
 
 class _OrderScreenState extends State<OrderScreen> {
-  bool _isLoading = false;
+
+  late Future _ordersFuture;
+
+  Future _obtainOrdersFuture() {
+    return Provider.of<Orders>(context, listen: false)
+        .fetchAndSetOrders();
+  }
+
 
   @override
   void initState() {
-    Future.delayed(Duration.zero).then((_) async {
-      setState(() {
-        _isLoading = true;
-      });
-      await Provider.of<Orders>(context, listen: false).fetchAndSetOrders();
-      setState(() {
-        _isLoading = false;
-      });
-    });
+    _ordersFuture = _obtainOrdersFuture();
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    final orders = Provider.of<Orders>(context).orders;
     return Scaffold(
       appBar: AppBar(
         title: const Text('Your Order'),
       ),
       drawer: const AppDrawer(),
-      body: _isLoading
-          ? const Center(child: CircularProgressIndicator())
-          : ListView.builder(
-              itemBuilder: (context, index) => OrderTile(
-                orderItem: orders[index],
-              ),
-              itemCount: orders.length,
-            ),
+      body: FutureBuilder(future: _ordersFuture,
+        builder: (BuildContext context, AsyncSnapshot<void> snapshot) {
+          if (snapshot.connectionState == ConnectionState.active) {
+            return const Center(child: CircularProgressIndicator());
+          } else {
+            if (snapshot.hasError) {
+              return const Center(child: Text("Something gone wrong."),);
+            } else {
+              return Consumer<Orders>(
+                builder: (BuildContext context, orderData, Widget? child) =>
+                    ListView.builder(
+                      itemBuilder: (context, index) =>
+                          OrderTile(
+                            orderItem: orderData.orders[index],
+                          ),
+                      itemCount: orderData.orders.length,
+                    ),
+              );
+            }
+          }
+        },),
     );
   }
 }
